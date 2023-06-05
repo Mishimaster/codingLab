2c9ce9a4bdbbe1ee29b78ecf3a1b2eb99c5b64d2
John Smith <luca.ginfo@proton.me>
2023-06-05 23:11:12 +0200

    React init

---
+++ resources/js/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="row my-5">
+                <div className="col-md-8 mx-auto">
+                    <h1> Add React js to laravel 10 with vite</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
